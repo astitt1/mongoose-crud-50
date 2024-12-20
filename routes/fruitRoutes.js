564335ff04c7dedf1253329b240a3d08af599e10
astitt1 <astitt@perscholas.org>
2024-12-20 09:19:59 -0500

    added delete route

--- routes/fruitRoutes.js
+++ routes/fruitRoutes.js
@@ -8,40 +8,50 @@ router.get("/", async (req, res) => {
     const allFruit = await Fruit.find({});
     res.json(allFruit);
   } catch (error) {
-    res.status(500).json({error: error.message})
+    res.status(500).json({ error: error.message });
   }
 });
 
 //create a new fruit
-router.post('/', async(req, res)=> {
-    try {
-        const createdFruit = await Fruit.create(req.body)
-        console.log(req.body)
-        res.json(createdFruit)
-    } catch (error) {
-        res.status(500).json({error: error.message})
-    }
-})
+router.post("/", async (req, res) => {
+  try {
+    const createdFruit = await Fruit.create(req.body);
+    console.log(req.body);
+    res.json(createdFruit);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 //show route - get 1 fruit
 
-router.get('/:id', async (req, res)=>{
-    try {
-      const singleFruit = await Fruit.findById(req.params.id)
-      res.json(singleFruit)
-    } catch (error) {
-        res.status(500).json({error: error.message})
-    }
-})
+router.get("/:id", async (req, res) => {
+  try {
+    const singleFruit = await Fruit.findById(req.params.id);
+    res.json(singleFruit);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 //edit route
-router.put('/:id', async(req, res)=>{
-    try {
-     const updatedFruit = await Fruit.findByIdAndUpdate(req.params.id, req.body)
-    } catch (error) {
-        res.status(500).json({error: error.message})
-    }
-})
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedFruit = await Fruit.findByIdAndUpdate(req.params.id, req.body);
+    res.json(updatedFruit);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
+router.delete("/:id", async (req, res) => {
+  try {
+    
+    const deletedFruit = await Fruit.findByIdAndDelete(req.params.id);
+    res.json(deletedFruit);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 module.exports = router;
